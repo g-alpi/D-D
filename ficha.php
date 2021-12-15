091d53a3192bf02f47916f512efec3b522f45980
edupedu101 <eduardvd2001@hotmail.com>
2021-12-15 19:17:10 +0100

    15/12/21

---
+++ ficha.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title></title>
+</head>
+<body id="ficha">
+	<div id="cabeceraFicha">
+		<div id="nombreFicha">
+			<h1 id="nombreFicha">Nombre Ficha</h1>
+		</div>
+		<div id="cabeceroDerecho">
+			<h3></h3>
+			<h3></h3>
+			<h3></h3>
+			<h3></h3>
+		</div>
+		<div class="flexHorizontal">
+			<div id="estadisticas"> 
+				<div></div> 
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div>Proficiencia</div>
+			<div>Velocidad</div>
+			<div>Hit Points</div>
+		</div>
+	</div>
+	<div id="centroFicha">
+		<div id="colIzquierdaFicha">
+			<div class="flex2Col">
+				<p>Fuerza</p>
+				<p>Destreza</p>
+				<p>Constitucion</p>
+				<p>Inteligencia</p>
+				<p>Sabiduria</p>
+				<p>Carisma</p>
+				<p>Tiradas de Salvacion</p>
+			</div>
+			<div>
+				<ul>
+					
+				</ul>
+				<p>Rasgos y Atributos</p>
+			</div>
+			<div>
+				<ul>
+					
+				</ul>
+				<p>Idiomas y proficiencias</p>
+			</div>
+		</div>
+		<div id="centroIzquierdaFicha">
+			<div class="flexHorizontal">
+				<div>Armadura</div>
+				<div>
+					<div>Defensas</div>
+					<div>Condiciones</div>
+				</div>
+			</div>
+			<div>
+				Habilidades
+				<table>
+					
+				</table>
+			</div>
+		</div>
+	</div>
+	<div id="pieFicha">
+		<div>
+			Historia
+			<ul>
+			
+			</ul>
+		</div>
+		<div>
+			Hechizos
+			<ul>
+				
+			</ul>
+		</div>
+		<div>
+			Equipo
+			<ul>
+				
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
